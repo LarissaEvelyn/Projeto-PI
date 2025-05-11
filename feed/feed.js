deea3b3c8e56a78f93cb596ca4175aed22a69658
Ziell-1 <josiell.matheus@academico.ifpb.edu.br>
2025-05-11 00:49:59 +0000

    Atualização

--- feed/feed.js
+++ feed/feed.js
@@ -6,7 +6,7 @@ function mostrarFormulario() {
 function adicionarPostagem() {
     //Pega conteúdo da caixa de testo e tira espaços desnecessários
     const texto = document.getElementById("novaPostagemTexto").value.trim();
-    if (!texto) return; //Esta linha vê se a mensagem esta vazia, similar aom (texto == "")
+    if (!texto) return;
 
     const feed = document.querySelector(".feed"); //div onde estão as postagens
 
