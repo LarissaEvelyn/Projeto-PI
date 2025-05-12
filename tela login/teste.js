c69fd68d22c42fa65e4f5fa9901067834b133aa6
LarissaEvelyn <larissa.evelyn@academico.ifpb.edu.br>
2025-05-11 22:55:51 -0300

    Update teste.js

--- tela login/teste.js
+++ tela login/teste.js
@@ -1,17 +1,1 @@
-export const login = [
-    {
-      
-      email: 'larissa.academico.ifpb.edu.br',
-      senha: 'atividade',
-    },
-    {
-      
-      email: 'jamylli.academico@ifpb.edu.br',
-      senha: 'aula',
-    },
-    {
-    
-      email: 'lara.academico@ifpb.edu.br',
-      senha: 'ifpb',
-    },
-  ];
+
