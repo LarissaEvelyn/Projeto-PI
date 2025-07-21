d368853d8a8dac1041c79b630f113bfc170a8c92
lararamalho <lara.ramalho@academico.ifpb.edu.br>
2025-07-20 21:20:22 -0300

    cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,41 @@
+// login.js
+
+export const login = [
+  {
+    email: 'hysia.milena@academico.ifpb.edu.br',
+    senha: 'escola'
+  },
+  {
+    email: 'larissa.evelyn@academico.ifpb.edu.br',
+    senha: 'atividade'
+  },
+  {
+    email: 'lara.ramalho@academico.ifpb.edu.br',
+    senha: 'ifpb'
+  }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('loginForm');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const emailInput = document.getElementById('email').value.trim();
+    const senhaInput = document.getElementById('password').value;
+
+    const usuarioEncontrado = login.find(
+      user => user.email === emailInput && user.senha === senhaInput
+    );
+
+    if (usuarioEncontrado) {
+      alert('Login bem-sucedido!');
+      // Salva no sessionStorage (opcional)
+      sessionStorage.setItem('usuarioLogado', emailInput);
+      // Redireciona para tela inicial
+      window.location.href = '../tela_inicial/tela_inicial.html';
+    } else {
+      alert('E-mail ou senha incorretos!');
+    }
+  });
+});
